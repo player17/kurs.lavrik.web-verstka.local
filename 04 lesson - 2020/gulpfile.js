028229e44ccf2fdd0b6a619042c5be34e452de3f
reandh <reandh@yandex.ru>
2022-04-27 21:17:59 +0500

    lesson 04 - start setting less

---
+++ 04 lesson - 2020/gulpfile.js
@@ -0,0 +1,84 @@
+const gulp = require('gulp');
+const del = require('del');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const browserSync = require('browser-sync').create();
+const gcmq = require('gulp-group-css-media-queries');
+const webp = require('gulp-webp');
+const less = require('gulp-less');
+
+
+//console.log(process.argv); // список всех аргуметов команды gulp
+let isDev = process.argv.includes('--dev');
+let isProd = !isDev;
+let isConcat = process.argv.includes('--concat');
+let isClean = process.argv.includes('--clean');
+let isSync = process.argv.includes('--sync');
+
+if(isDev) {
+    console.log('Modus Dev');
+} else {
+    console.log('Modus Prod');
+}
+
+function clean() {
+    return del('./build/*');
+}
+
+function html() {
+    return gulp.src('./src/**/*.html')
+        // silense
+        .pipe(gulp.dest('./build/'))
+        .pipe(gulpIf(isSync,browserSync.stream()));
+}
+
+function styles() {
+    //return gulp.src('./src/css/**/*.css')
+    return gulp.src(['./src/css/main.less'])
+        // pipe: minify, prefix, join
+        .pipe(gulpIf(isDev,sourcemaps.init()))
+        //.pipe(gulpIf(isConcat,concat('main.less')))
+        .pipe(less())
+        .pipe(autoprefixer({}))
+        .pipe(gcmq())
+        .pipe(gulpIf(isClean, cleanCSS({
+            level: 2
+        })))
+        .pipe(gulpIf(isDev,sourcemaps.write()))
+        .pipe(gulp.dest('./build/css/'))
+        .pipe(gulpIf(isSync,browserSync.stream()));
+}
+
+function images() {
+    return gulp.src('./src/img/**/*')
+        .pipe(gulp.dest('./build/img/'));
+}
+
+function imagesToWebp() {
+    return gulp.src('./src/img/**/*')
+        .pipe(webp())
+        .pipe(gulp.dest('./build/img/'));
+}
+
+function watch() {
+    if(isSync) {
+        browserSync.init({
+            server: {
+                baseDir: "./build/"
+            }
+        });
+    }
+    gulp.watch('./src/**/*.html', html);
+    //gulp.watch('./src/css/**/*.css', styles);
+    gulp.watch('./src/css/**/*.less', styles);
+}
+
+let build = gulp.parallel(html, styles, images, imagesToWebp);
+let buildWithClean = gulp.series(clean, build);
+let dev = gulp.series(buildWithClean, watch);
+
+gulp.task('build', buildWithClean);
+gulp.task('watch', dev);
---
+++ 04 lesson - 2020/listCMDnpm.md
@@ -0,0 +1,66 @@
+### node.js cmd 
+> `https://www.npmjs.com/` все пакеты npm
+
+> `npm` пакетный менеджер для node.js
+
+> `cd /d d:\OpenServer20220222\domains\kurs.lavrik.web-verstka.local` 
+смена директории
+
+> `npm ls` список установленых модулей
+> `npm ls --depth=1`
+> `npm ls --depth=0 -g`
+
+> `npm init -y` инициализация package.json файла 
+> с конфигами проекта, `-y` тихий режим
+
+### Установка и удаление пакетов/модулей
+> `npm i jquery` пример установки jquery
+
+> `npm r jquery` пример удаления jquery
+
+> `npm i gulp -D` установка в рамках dev разработки, 
+> в package.json файле директория devDependencies
+
+> `npm i gulp-cli -g` установка глобально
+
+> `npm i gulp-webpack -g`
+
+> `npm i del -D` модуль для удаления папок и файлов
+
+> `npm i gulp-concat -D` модуль для объединения файлов
+
+> `npm i gulp-autoprefixer -D` модуль для генерации префиксов (кросбраузерность)
+
+> `npm i gulp-clean-css -D` минификация css
+
+> `npm i gulp-sourcemaps -D` карта минифицированого кода
+
+> `npm i browser-sync -D` модуль отслеживания изменений в реальном времени
+
+> `npm i gulp-if -D` модуль для различных режимов запуска
+> 
+> `npm i gulp-webp -D` модуль для сжатия картинок
+
+### Конфигурация gulp
+> `npm i` стартует установку всех пакетов из `package.json`
+> `package-lock.json` содержит список всех зависимостей пакетов (не обязателен, но желателен, для послной совместимости всех пакетов между собой)
+> `gulpfile.js` файл конфигурации в корень проекта 
+> (запускается командой `gulp` в cmd)
+> > `gulp build` пример простой команды, запуска проекта ч/з `gulp.task('build', ...)`
+> > сконфигрурированной в `gulpfile.js`
+
+> `gulp build` запуск проекта на продуктив
+
+> `package.json` раздел `script` пишем скрипты запуска
+> > `npm run dev` запуск gulp watch настроеной в gulpfile.js 
+
+### Start project
+> `npm run dev` запуск в режиме разработчика ч/з npm package.json[scripts]
+> `npm run build` запуск в режиме продукта ч/з npm package.json[scripts]
+> `gulp build --dev --concat --sync --clean` запуск ч/з gulp gulpfile.js
+
+### Pre-proccess
+> `gulpfile.js` подключаем gulp-less в файле
+> > `npm i gulp-less -D`
+
+
