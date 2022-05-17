7161f58e7ddb4a115683a5623e7ac97c775e24e7
reandh <reandh@yandex.ru>
2022-05-18 03:50:12 +0500

    lesson 06 - Smart Grid Custom

---
+++ 06 lesson - 2020/smartgrid.js
@@ -0,0 +1,34 @@
+module.exports = {
+	columns: 12,
+	offset: "30px",
+	container: {
+		maxWidth: "950px",
+		fields: "30px"
+	},
+	breakPoints: {
+		md: {
+			width: "900px",
+			fields: "15px"
+		},
+	   sm: {
+	      width: "720px"
+	   },
+	   xs: {
+	      width: "576px",
+	      /*
+	      offset: "5px",
+	      fields: "5px"
+	      */
+	   },
+	   xxs: {
+	      width: "400px"
+	   }
+	},
+	mobileFirst: false,
+	tab: '\t',
+	/*
+	mixinNames: {
+   	container: 'container'
+	},
+	detailedCalc: true*/
+}
